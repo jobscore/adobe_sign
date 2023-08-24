bd5ba9de90ed54170f29d3af8bde5c207f667d1d
Glauber Batista <glauber@jobscore.com>
2023-08-24 14:15:45 -0300

    test: re-record all vcrs and improve tests to make sure it's working

--- test/documents_test.rb
+++ test/documents_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 describe 'Document Test' do
   let(:base_path) { 'https://api.na2.echosign.com/' }
   let(:file) { File.new('test/fixtures/agreement.pdf', 'rb') }
+  let(:record_mode) { :none }
 
   describe '#upload' do
     it 'should return response error when parameters is not send properly' do
       token = 'INVALID_TOKEN'
 
-      VCR.use_cassette('documents_upload_invalid') do
+      VCR.use_cassette('documents_upload_invalid', record: record_mode) do
         response = AdobeSign::Documents.new(base_path, token).upload(file, 'document.pdf')
 
         assert_equal :unauthorized, response.status
@@ -20,7 +21,7 @@ describe 'Document Test' do
     it 'should response a valid data when parameters is send properly and document was uploaded gracefully' do
       token = 'VALID_ACESSS_TOKEN'
 
-      VCR.use_cassette('documents_upload_valid') do
+      VCR.use_cassette('documents_upload_valid', record: record_mode) do
         response = AdobeSign::Documents.new(base_path, token).upload(file, 'document.pdf')
         response_data = { 'transientDocumentId' => 'NEW_TRANSIENT_DOCUMENT_ID' }
 
